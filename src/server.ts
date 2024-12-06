67b031594570dbb3636b559306ae0a6b3cac001a
Aziz-boy <shavgoniaziz@gmail.com>
2024-12-06 18:04:18 +0500

    BRR: Initial Setup

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import moment from "moment";
+
+ //Ecmajs ni ishlatyapmiz
+console.log("Server is running...");
+
+const currentTime = moment().format("YYYY-MM-DD");
+console.log(currentTime);
+
+
+const person: string = "Adam";
+const count: number = 100;
+ 
+//npm run build orqali biz dist folderga ozimizni typescriptni jsga o'girilib beriladi 
