9cc2c39e713f652890fc9544fec061c9be4c5bb2
Aziz-boy <shavgoniaziz@gmail.com>
2024-12-07 10:56:28 +0500

    Task G added

--- train.js
+++ train.js
@@ -13,25 +13,12 @@ Va bu 21 soni arrayning tarkibidagi birinchi eng katta son hisobladi va bizga un
 */
 
 function getHighestIndex(arr) { 
+    const largest = Math.max(...arr);
+    return arr.indexOf(largest); 
+}  
 
-  if (arr.every(Number.isInteger)) {
-    let maxIndex = 0; 
-    for (let i = 1; i < arr.length; i++) { 
-      if (arr[i] > arr[maxIndex]) {
-        maxIndex = i; 
-      }
-    }
-    return maxIndex; 
+  
 
-    
-
-
-  } else {
-    console.log('Your array is not consist of integers !');
-  }
-
-}
-
-const result = getHighestIndex([5, 21, 12, 21 ,8]);
+const result = getHighestIndex([5, 21, 12, 21 ,8,]);
 
 console.log(result);
