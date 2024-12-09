fc6886f24ae9ce326318a6f0cfbeddee3bc04835
Aziz-boy <shavgoniaziz@gmail.com>
2024-12-09 14:43:37 +0500

    feat: Express Integrations

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import  express, { urlencoded } from "express";
+import path from 'path';
+/*** 1- ENTERENCE ***/
+const app = express();
+// console.log(__dirname)
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({extended: true})); //Traditional 
+app.use(express.json()); //REST api sifatida request bolayotgan json datalarni otkazsihga ruxsta beryapmiz
+/*** 2- SESSIONS ***/
+/*** 3- VIEWS ***/
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', "ejs");
+/*** 4- ROUTERS ***/
+
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,18 @@
  //Ecmajs ni ishlatyapmiz
 console.log("Server is running...");
-
 import dotenv from 'dotenv';
 dotenv.config();
-
 import mongoose from 'mongoose';
+import app from "./app";
 
 mongoose
    .connect(process.env.MONGO_URL as string, {})
    .then((data) => {
     console.log("MongoDB Connection Succeed");
     const PORT = process.env.PORT ?? 3003;
+    app.listen(PORT, function() {
+      console.log(`The server is Running Successfully on: ${PORT}`)
+    })
    })
    .catch((err) => console.log("ERROR on Connection MongoDB", err))
 
