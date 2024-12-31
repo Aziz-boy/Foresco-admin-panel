10a344aa2369f11c515c927756951ff4d2b980f8
Aziz-boy <shavgoniaziz@gmail.com>
2024-12-31 07:32:48 +0500

    feat: develop updateChosenUser api

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -129,12 +129,17 @@ restaurantController.getUsers = async (req: Request, res: Response) => {
     }
 };
 
-restaurantController.updateChosenUser = (req: Request, res: Response) => {
+restaurantController.updateChosenUser = async (req: Request, res: Response) => {
     try {
         console.log("updateChosenUser");
-    } catch (err) {
-        console.log("Error, updateChosenUser:", err);
+        const result = await memberService.updateChosenUser(req.body);
 
+        res.status(HttpCode.OK).json({ data: result });
+    } catch (err) {
+        if(err instanceof Errors) res.status(err.code).json(err);
+        else res.status(Errors.standard.code).json(Errors.standard);
+
+        
     }
 };
 
--- src/router-admin.ts
+++ src/router-admin.ts
@@ -44,7 +44,17 @@ routerAdmin.post('/product/:id',
 
 /*** User ***/   
 
-routerAdmin.get('/user/all', restaurantController.verifyRestaurant, restaurantController.getUsers);
+routerAdmin.get(
+    '/user/all', 
+    restaurantController.verifyRestaurant, 
+    restaurantController.getUsers
+);
+
+routerAdmin.post(
+    '/user/edit', 
+    restaurantController.verifyRestaurant, 
+    restaurantController.updateChosenUser
+);
 
 export default routerAdmin; 
 
