ed676b3372a3b522fea52ef03a5c46b5938f588d
Aziz-boy <shavgoniaziz@gmail.com>
2025-01-04 20:16:44 +0500

    feat: EJS - Frontend signup jarayonini develop qilamiz

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,8 +1,32 @@
-const { relativeTimeRounding } = require("moment");
-
 console.log("Signup frontend javascript file");
 
-$(function() {});
+$(function() {
+    const fileTarget = $(".file-box .upload-hidden");
+    let filename;
+
+    fileTarget.on("change", function(){
+      if(window.FileReader) {
+        const uploadFile = $(this)[0].files[0];
+        console.log("uploadFile: " , uploadFile)
+        const fileType = uploadFile["type"];
+        const validImageType = ["image/jpg", "image/jpeg", "image/png"];
+        if(!validImageType.includes(fileType)){
+            alert("Please upload only jpeg, jpg and png image formats");
+            
+        } else {
+            if(uploadFile) {
+               console.log(URL.createObjectURL(uploadFile));
+               $(".upload-img-frame")
+               .attr("src", URL.createObjectURL(uploadFile))
+               .addClass("success");
+            }
+            filename = $(this)[0].files[0].name;
+        }
+
+        $(this).siblings(".upload-name").val(filename); 
+      }
+    });
+});
 
 
 function validateSignupForm() {
@@ -28,4 +52,15 @@ function validateSignupForm() {
     alert("Passwords do not match, Please check your password");
     return false;
    } 
+
+
+
+   const memberImage = $(".member-image").get(0).files[0] ? $(".member-image").get(0).files[0].name 
+   : null; 
+
+   if(!memberImage) { 
+    alert("Please insert image");
+    return false;
+   }    
+
 }
