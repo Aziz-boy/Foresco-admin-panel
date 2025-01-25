ef5237c2ac721bb6ce52eccd9a50d7a150bfafb0
Aziz-boy <shavgoniaziz@gmail.com>
2025-01-25 10:59:21 +0500

    fix: modify getMemberDetail business logic

--- src/controllers/member.controller.ts
+++ src/controllers/member.controller.ts
@@ -3,7 +3,12 @@ import { NextFunction, Request, Response } from "express";
 import { T } from "../libs/types/common";
 import MemberService from "../models/Member.service";
 import AuthService from "../models/Auth.service";
-import { ExtendedRequest, LoginInput, Member, MemberInput } from "../libs/types/member";
+import {
+  ExtendedRequest,
+  LoginInput,
+  Member,
+  MemberInput,
+} from "../libs/types/member";
 import Errors, { HttpCode, Message } from "../libs/Errors";
 import { AUTH_TIMER } from "../libs/config";
 
@@ -57,19 +62,29 @@ memberController.login = async (req: Request, res: Response) => {
   }
 };
 
-
 memberController.logout = (req: ExtendedRequest, res: Response) => {
   try {
-    console.log("logout")
-    res.cookie("accessToken", null, {maxAge:0, httpOnly:true});
+    console.log("logout");
+    res.cookie("accessToken", null, { maxAge: 0, httpOnly: true });
     res.status(HttpCode.OK).json({ logout: true });
-  } catch(err) {
+  } catch (err) {
     console.log("Error, logout:", err);
     if (err instanceof Errors) res.status(err.code).json(err);
     else res.status(Errors.standard.code).json(Errors.standard);
   }
-}
+};
 
+memberController.getMemberDetail = async (req: ExtendedRequest, res: Response) => {
+  try {
+    console.log("getMemberDetail");
+    const result = await memberService.getMemberDetail(req.member);
+    res.status(HttpCode.OK).json(result);
+  } catch (err) {
+    console.log("Error, getMemberDetail:", err);
+    if (err instanceof Errors) res.status(err.code).json(err);
+    else res.status(Errors.standard.code).json(Errors.standard);
+  }
+};
 
 memberController.verifyAuth = async (
   req: ExtendedRequest,
@@ -90,7 +105,8 @@ memberController.verifyAuth = async (
   }
 };
 
-memberController.retrieveAuth = async ( //bu mantigimiz kirib kelayotgan requestni kimligini tekshiradi. login bolagn bolsa datasini olib beradi login bolmagan bolsa ham otkazib yuboradi 
+memberController.retrieveAuth = async (
+  //bu mantigimiz kirib kelayotgan requestni kimligini tekshiradi. login bolagn bolsa datasini olib beradi login bolmagan bolsa ham otkazib yuboradi
   req: ExtendedRequest,
   res: Response,
   next: NextFunction
@@ -99,7 +115,7 @@ memberController.retrieveAuth = async ( //bu mantigimiz kirib kelayotgan request
     let member = null;
     const token = req.cookies["accessToken"]; // bizni requestimizdagi cookini ichidan access tokenni tekshiradi
     if (token) req.member = await authService.checkAuth(token); //agar mavjud bolsa payloadimizni memberga yukalb beradi     //TODO shu yerga await qo'ymasa nima boladi
-    
+
     next(); //shu bosqichda error bolsa hm akeyingisizga otkazish manqtiqi
   } catch (err) {
     console.log("Error, retrieveAuth:", err);
